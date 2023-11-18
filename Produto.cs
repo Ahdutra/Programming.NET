f99a6ac058b3b7555ea7cc1694882535e3c0bc9c
GAIT - 11 <GAIT - 11@DESKTOP-UVNMDH6>
2023-11-18 15:07:29 -0400

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_.NET_Exercícios
+{
+        //classe produto
+        public class Produto
+        {
+            public string? Nome { get; set; }
+            public double Preço { get; set; }
+            public string? Descrição { get; set; }
+
+        }
+    // criando o method Imprimir detalhes
+    public static class ProdutoDetalhes
+    {
+        public static string ImprimirDetalhes(this Produto produto)
+        {
+            return $"Nome:{produto.Nome}\nPreço:{produto.Preço}\nDescrição: {produto.Descrição}";
+        }
+
+
+        //aplicando desconto
+        public static double AplicarDesconto(this Produto produto, double percentualDesconto)
+        {
+            double desconto = produto.Preço * (percentualDesconto / 100);
+            return produto.Preço - desconto;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Prog_.NET_Exercícios
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            //Produto
+            Produto meuProduto = new Produto
+            {
+                Nome = "caderno",
+                Preço = 20.00,
+                Descrição = "caderno universitário 20 matérias",
+            };
+            //detalhes
+            Console.WriteLine("Detalhes do Produto: \n" + meuProduto.ImprimirDetalhes);
+
+            double percentualDesconto = 5.0;
+            double precoComDesconto = meuProduto.AplicarDesconto(percentualDesconto);
+
+            Console.WriteLine($"(\nDetalhes do Produto com {percentualDesconto}% de desconto:\n + $Preço com desconto: {precoComDesconto}:)");
+
+        }
+    }
+}
